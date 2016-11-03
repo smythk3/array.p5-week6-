bef7346ec416b29ac99d4c5c1a3bef7a49e2276e
smythk3 <smythk3@tcd.ie>
2016-11-03 15:40:46 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var myArray = [1, 2, 4, 6, 7, -5]; // 6 numbers. array is 6 in size
+// the order of elements in an array is significant: each element has an INDEX, which starts at 0!!!
+
+
+function setup() {
+  println(myArray[2]);
+  
+  
+}
+
+function draw() {
+  
+
+}
